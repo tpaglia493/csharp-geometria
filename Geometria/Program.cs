60e51e7b7638dd85b1e9d6225500a4352adf43c0
tpaglia493 <tommasopaglia93@outlook.it>
2023-04-17 13:42:54 +0200

    added rectangle class  and made instance in main program

--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -19,3 +19,17 @@ BONUS:
 Aggiungere alla classe Rettangolo un metodo “disegna” che disegna in console il rettangolo con le sue dimensioni, 
 ossia tanti “—” (due trattini) orizzontali quanto è grande la sua base e tanti ‘|’ verticali quanto e grande la sua altezza.
 */
+
+//---------------------------- MAIN PROGRAM ------------------------------
+using Geometria;
+
+Console.Write("Please insert the heigth of the rectangle:");
+int rectangleHeigth = int.Parse(Console.ReadLine());
+
+Console.Write("Please insert the length of the base of the rectangle:");
+int rectangleBaseLength = int.Parse(Console.ReadLine());
+
+Rectangle newRectangle = new Rectangle(rectangleBaseLength, rectangleHeigth);
+
+Console.WriteLine("The perimeter is " + newRectangle.GetPerimeter()+"cm"); ;
+Console.WriteLine("The area is " + newRectangle.GetArea()+"cm^2");
---
+++ Geometria/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rectangle
+    {//ATTRIBUTES
+        public int baseLength;
+        public int height;
+        public int area;
+        public int perimeter;
+
+        //STATES
+
+
+        //CONSTRUCTOR
+        public Rectangle(int baseLength, int height)
+        {
+            this.baseLength = baseLength;
+            this.height = height;
+        }
+
+    //METHODS   
+        public int GetArea() 
+        {
+            area = (baseLength * height);
+            return area; 
+        }
+        public int GetPerimeter() {
+            perimeter = (baseLength + height) * 2;
+            return perimeter;
+        }
+    }
+}
+
